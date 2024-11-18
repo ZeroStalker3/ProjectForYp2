12b46d410594f16b24482b68c1817ef71ed33a66
ZeroStalker3 <stalker_mr@list.ru>
2024-11-18 14:30:30 +0900

    Добавьте файлы проекта.

---
+++ Model/Requests.cs
@@ -0,0 +1,17 @@
+﻿namespace ProjectForYp2.Model
+{
+    internal class Requests
+    {
+        public int RequestID { get; set; }
+        public DateOnly StartDate { get; set; }
+        public int OrgTechType { get; set; }
+        public string OrgTechManufacture { get; set; } = null!;
+        public string OrgTechModel { get; set; } = null!;
+        public string OrgTechNumber { get; set; } = null!;
+        public string Id_requestStatus { get; set; } = null!;
+        public DateOnly CompletionDate { get; set; }
+        public string RepairParts { get; set; } = null!;
+        public int masterID { get; set; }
+        public int clientID { get; set; }
+    }
+}
---
+++ Model/User.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectForYp2.Model
+{
+    internal class User
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = null!;
+        public string FirstName { get; set; } = null!;
+        public string? LastName { get; set; }
+        public decimal Phone { get; set; }
+        public string Login { get; set; } = null!;
+        public string Password { get; set; } = null!;
+        public Type Type { get; set; } = null!; 
+
+    }
+}
