7a274b719d5156598625bd8a777a9eaba1c5b358
ZeroStalker3 <stalker_mr@list.ru>
2024-11-29 11:22:43 +0900

    Update

--- pages/PageEditRequest.xaml.cs
+++ pages/PageEditRequest.xaml.cs
@@ -1,25 +1,9 @@
-﻿using Azure.Core;
-using Microsoft.EntityFrameworkCore;
-using ProjectForYp2.classes;
+﻿using ProjectForYp2.classes;
 using ProjectForYp2.data;
 using ProjectForYp2.Model;
 using ProjectForYp2.pages.SecondPage;
-using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Linq;
-using System.Security.Cryptography;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace ProjectForYp2.pages
 {
@@ -30,22 +14,28 @@ namespace ProjectForYp2.pages
     {
         classdata data = new classdata();
         Requests requests = new Requests();
-        
+
+
         int reqId;
 
         public PageEditRequest()
         {
             InitializeComponent();
 
+            data.GetRequests();
             UserGrid.ItemsSource = data.userContext.Requests.Local.ToList();
 
             EditFrame.frmEdit = Frmedit;
+
+            welldonereq.Text = "Выполнено: " + Convert.ToString(data.userContext.Requests.Where(s => s.Id_Statys.Name == "Готова к выдаче").Count());
+            workreq.Text = "В работе: " + Convert.ToString(data.userContext.Requests.Where(s => s.Id_Statys.Name == "В процессе ремонта").Count());
+            newreq.Text = "Новая заявка: " + Convert.ToString(data.userContext.Requests.Where(s => s.Id_Statys.Name == "Новая заявка").Count());
         }
 
         private void edit_Click(object sender, RoutedEventArgs e)
         {
             var req = (sender as Button).DataContext;
-            
+
             foreach (var item in data.userContext.Requests.Local)
             {
                 if (item == req)
@@ -77,13 +67,41 @@ namespace ProjectForYp2.pages
             data.GetRequests();
             Requests request = (Requests)(sender as Button).DataContext;
 
-            DateTime date = Convert.ToDateTime(request.StartDate);
-            DateTime date1 = Convert.ToDateTime(request.CompletionDate);
+            DateTime date = request.StartDate.ToDateTime(TimeOnly.MinValue);
+            DateTime date1 = request.CompletionDate.
+                GetValueOrDefault(DateOnly.MinValue).ToDateTime(TimeOnly.MinValue);
+            data.GetComments();
             var dateTime = date1 - date;
+            var com = data.userContext.Comments.Where(x => x.reqId.Id == reqId).FirstOrDefault();
+            string reppart = "";
+            string coment;
+            if (com != null)
+            {
+                coment = com.Message;
+            }
+            else
+            {
+                coment = "Мастер комментарий не оставил";
+            }
 
-            MessageBox.Show($"Количество дней: {Convert.ToString
-                (Math.Round((Convert.ToDateTime(date1) - Convert.ToDateTime(date)).TotalDays))}\n Используемые детали: {request.RepairParts}", "Отчет"
+            if (request.RepairParts != "")
+            { 
+                reppart = request.RepairParts; 
+            }
+            else
+            { 
+                reppart = "Мастер не использовал доп. части"; 
+            }
+            MessageBox.Show($"Количество дней: {dateTime.TotalDays}\nИспользуемые детали: {reppart}\nКоментарий мастера: {coment}", "Отчет"
                 , MessageBoxButton.OK, MessageBoxImage.Information);
+        }
+
+        private void sort1_SelectionChanged(object sender, RoutedEventArgs e)
+        {
+            string select = Convert.ToString(sort1.Text.ToUpper());
+            UserGrid.ItemsSource = data.userContext.Requests.Where(x =>
+            x.OrgTechManufacture.ToUpper().Contains(select))
+                .ToList();
         }
     }
 }
